7d2c042a148b3f7bb0d6a646cc20d4a4b951d7fa
antoine duvauchelle <antoine@goodmorningcoconut.co>
2018-08-21 11:13:12 +0200

    fixed card intell and applied some out-of-the-box styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_123017) do
+ActiveRecord::Schema.define(version: 2018_08_21_084411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2018_08_20_123017) do
     t.string "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "activity"
+    t.string "category"
     t.index ["activity_id"], name: "index_moments_on_activity_id"
     t.index ["user_id"], name: "index_moments_on_user_id"
   end
